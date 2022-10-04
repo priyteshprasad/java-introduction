244654b553f3e980b26daeb07792ad250df072cd
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2022-10-04 22:32:27 +0530

    introduction to java, comment, println & print, datatypes

---
+++ introduction.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      //println changes the line 
+      System.out.println("Hello World");
+      
+      //.print doesnt change the line
+      System.out.print("Hello");
+      System.out.println("Priytesh");
+      
+      //data type: int-> number without by default
+      int a = 13;
+      System.out.println(a);
+      
+      //data type: long-> big number, decimal number is double by default
+      long l = 1234567891234567891l;
+      System.out.println(l);
+      
+      //float-> store small decimal number
+      float f = 123.45678912345f;
+      System.out.println(f);
+      
+      //double ->store big decimal numbers
+      double d = 13.734;
+      System.out.println(d);
+      
+      //String -> always in "double quotes", long list of characters
+      String s = "hellp";
+      System.out.println(s);
+      
+      //char -> '1','c','*',' '
+      char c = '@';
+      System.out.println(c);
+      
+      //bool -> true or false 
+      boolean b = true;
+      System.out.println(b);
+      
+      System.out.println("'");
+      
+      
+      
+      
+      
+      
+  }
+}
